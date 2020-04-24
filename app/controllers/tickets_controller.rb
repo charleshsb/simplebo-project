d04b812ab8befd2ae345d541dc119d2bf011cfbb
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-04-24 18:32:53 +0200

    refacto colors update ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,7 @@
 class TicketsController < ApplicationController
   def create
-    # @ticket = Ticket.new(#passer les faker)
-    # @ticket.user = #mettre le user qui a le moins de tickets
+    # @ticket = Ticket.new(passer les faker)
+    # @ticket.user = mettre le user qui a le moins de tickets
     @ticket.save
   end
 
@@ -15,12 +15,26 @@ class TicketsController < ApplicationController
 
   def show
     @ticket = Ticket.find(params[:id])
-    @comments = Comment.where(ticket: @ticket)
+    @comments = Comment.where(ticket: @ticket).order("created_at DESC")
+    @comment = Comment.new
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+    @ticket.update(done: true)
+    @ticket.save
+    redirect_to ticket_path(@ticket)
   end
 
   def destroy
     @ticket = Ticket.find(params[:id])
     @ticket.destroy
     redirect_to tickets_path
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:done)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :tickets, only: [:create, :index, :show, :destroy]
+  resources :tickets, only: [:create, :index, :show, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
 end
