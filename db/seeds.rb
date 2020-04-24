840c41ace1081b0e3b8e9b8cea47dd687482adf5
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-04-24 10:56:39 +0200

    users seed complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,8 @@ Ticket.destroy_all
 Comment.destroy_all
 
 jeremy = User.create(email: "jeremy.kenigsman@simplebo.fr", password: "qsdfghjklm", first_name: "Jérémy", last_name: "Kenigsman")
+nicolas = User.create(email: "nicolas.leroy@simplebo.fr", password: "123456", first_name: "Nicolas", last_name: "Leroy")
+charles = User.create(email: "charles.stas@simplebo.fr", password: "123456", first_name: "Charles", last_name: "Stas")
+sophie = User.create(email: "sophie.davant@simplebo.fr", password: "123456", first_name: "Sophie", last_name: "Davant")
 
 puts "seed completed"
