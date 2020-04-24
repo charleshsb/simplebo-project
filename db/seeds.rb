8e86f2ac1d2fc02e1629f8f37419b12af17d2515
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-04-24 10:53:51 +0200

    seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "starting seed"
+User.destroy_all
+Ticket.destroy_all
+Comment.destroy_all
+
+jeremy = User.create(email: "jeremy.kenigsman@simplebo.fr", password: "qsdfghjklm", first_name: "Jérémy", last_name: "Kenigsman")
+
+puts "seed completed"
