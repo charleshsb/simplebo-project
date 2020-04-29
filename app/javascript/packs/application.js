fa5abff1e6de54e8c93af592511a3b1b4c7bcd95
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-04-29 16:17:45 +0200

    js filter

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,16 @@
 import "bootstrap";
+
+const filter = (event) => {
+  console.log(event.target.value);
+  const search = event.target.value.toLowerCase()
+  document.querySelectorAll('.cards').forEach((product) => {
+    const text = product.innerText.toLowerCase();
+    if (text.match(search)) {
+        product.style.display="block"
+    } else {
+        product.style.display="none"
+    }
+  })
+}
+
+document.querySelector('#search-bar').addEventListener("keyup", filter);
