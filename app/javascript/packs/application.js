c2eb82ded1553de11d6492ce784d6990b7fc624c
charles henry stas boyeldieu <ch.boyeldieu@gmail.com>
2020-04-29 16:52:57 +0200

    refacto js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,13 @@
 import "bootstrap";
 
 const filter = (event) => {
-  console.log(event.target.value);
   const search = event.target.value.toLowerCase()
   document.querySelectorAll('.cards').forEach((product) => {
     const text = product.innerText.toLowerCase();
     if (text.match(search)) {
-        product.style.display="block"
+      product.style.display="block"
     } else {
-        product.style.display="none"
+      product.style.display="none"
     }
   })
 }
